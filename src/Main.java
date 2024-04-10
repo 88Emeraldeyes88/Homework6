3e3708b2ce9d9eaa09118d1da38bcb5489c0a840
88Emeraldeyes88 <koval.t87@gmail.com>
2024-04-11 00:18:26 +0400

    Сделано дз

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,68 @@ public class Main {
             System.out.println("i = " + i);
         }
 
+        System.out.println("Задача 1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("Задача 2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("Задача 3");
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println("Чётные числа " + i);
+        }
+
+        System.out.println("Задача 4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("Задача 5");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным.");
+        }
+
+        System.out.println("Задача 6");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
+
+        System.out.println("Задача 8");
+        int savings = 29000;
+        int sum = 0;
+        for (int month = 1; month <= 12; month++) {
+            sum = sum + savings;
+            System.out.println("Месяц " + month + " сумма накоплений равна " + sum + " рублей." );
+        }
+
+        System.out.println("Задача 9");
+        int saving = 29000;
+        float sums = 0;
+        for (int month = 0; month <= 12; month++) {
+            sums = sums + sums/100;
+            sums = sums + saving;
+            System.out.println("Месяц " + month + " сумма накоплений равна " + sums + " рублей.");
+        }
+
+        System.out.println("Задача 10");
+        int number = 2;
+        for (int t = 1; t <= 10; t++) {
+            System.out.println(number + "*" + t + "=" + number * t);
+        }
+
+
+
+
+
 
     }
 }
